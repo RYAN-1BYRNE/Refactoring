1fe200166141bf587a71dff07b0f1df019483c91
RYAN-1BYRNE <18ryanbyrne@gmail.com>
2019-05-06 18:47:43 +0530

    Reformat all files

--- src/SearchByIdDialog.java
+++ src/SearchByIdDialog.java
@@ -1,85 +1,75 @@
 /*
- * 
+ *
  * This is the dialog for Employee search by ID
- * 
+ *
  * */
 
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.GridLayout;
+import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.border.EtchedBorder;
-
 public class SearchByIdDialog extends JDialog implements ActionListener {
-	private EmployeeDetails parent;
-	private JButton search, cancel;
-	private JTextField searchField;
-	// constructor for SearchByIdDialog 
-	public SearchByIdDialog(EmployeeDetails parent) {
-		setTitle("Search by Surname");
-		setModal(true);
-		this.parent = parent;
-		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    private EmployeeDetails parent;
+    private JButton search, cancel;
+    private JTextField searchField;
 
-		JScrollPane scrollPane = new JScrollPane(searchPane());
-		setContentPane(scrollPane);
+    // constructor for SearchByIdDialog
+    public SearchByIdDialog(EmployeeDetails parent) {
+        setTitle("Search by Surname");
+        setModal(true);
+        this.parent = parent;
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-		getRootPane().setDefaultButton(search);
-		
-		setSize(500, 190);
-		setLocation(350, 250);
-		setVisible(true);
-	}// end SearchByIdDialog
-	
-	// initialize search container
-	public Container searchPane() {
-		JPanel searchPanel = new JPanel(new GridLayout(3, 1));
-		JPanel textPanel = createTextPanel();
-		JPanel buttonPanel = createButtonPanel();
-		searchPanel.add(new JLabel("Search by ID"));
-		searchPanel.add(textPanel);
-		searchPanel.add(buttonPanel);
+        JScrollPane scrollPane = new JScrollPane(searchPane());
+        setContentPane(scrollPane);
 
-		return searchPanel;
-	}// end searchPane
+        getRootPane().setDefaultButton(search);
 
-	private JPanel createButtonPanel() {
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.add(search = new JButton("Search"));
-		search.addActionListener(this);
-		search.requestFocus();
-		buttonPanel.add(cancel = new JButton("Cancel"));
-		cancel.addActionListener(this);
-		return buttonPanel;
-	}
+        setSize(500, 190);
+        setLocation(350, 250);
+        setVisible(true);
+    }// end SearchByIdDialog
 
-	// create text panel
-	public JPanel createTextPanel() {
-		JLabel searchLabel;
-		JPanel textPanel = new JPanel();
-		textPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
-		textPanel.add(searchLabel = new JLabel("Enter ID:"));
-		searchLabel.setFont(this.parent.font1);
-		textPanel.add(searchField = new JTextField(20));
-		searchField.setFont(this.parent.font1);
-		searchField.setDocument(new JTextFieldLimit(20));
-		return textPanel;
-	}
+    // initialize search container
+    public Container searchPane() {
+        JPanel searchPanel = new JPanel(new GridLayout(3, 1));
+        JPanel textPanel = createTextPanel();
+        JPanel buttonPanel = createButtonPanel();
+        searchPanel.add(new JLabel("Search by ID"));
+        searchPanel.add(textPanel);
+        searchPanel.add(buttonPanel);
 
-	// action listener for save and cancel button
-	public void actionPerformed(ActionEvent e) {
-		// if option search, search for Employee
-		SearchByDialog.actionPerformed(e, search, searchField, cancel, this.parent);
-	}
+        return searchPanel;
+    }// end searchPane
+
+    private JPanel createButtonPanel() {
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(search = new JButton("Search"));
+        search.addActionListener(this);
+        search.requestFocus();
+        buttonPanel.add(cancel = new JButton("Cancel"));
+        cancel.addActionListener(this);
+        return buttonPanel;
+    }
+
+    // create text panel
+    public JPanel createTextPanel() {
+        JLabel searchLabel;
+        JPanel textPanel = new JPanel();
+        textPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+        textPanel.add(searchLabel = new JLabel("Enter ID:"));
+        searchLabel.setFont(this.parent.font1);
+        textPanel.add(searchField = new JTextField(20));
+        searchField.setFont(this.parent.font1);
+        searchField.setDocument(new JTextFieldLimit(20));
+        return textPanel;
+    }
+
+    // action listener for save and cancel button
+    public void actionPerformed(ActionEvent e) {
+        // if option search, search for Employee
+        SearchByDialog.actionPerformed(e, search, searchField, cancel, this.parent);
+    }
 }// end class searchByIdDialog
